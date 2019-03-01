d0b0c8e1d95b4ddd9dd2ad2f45cea72ece3fbd0f
maruware <me@maruware.com>
2019-03-01 18:12:15 +0900

    リファクタ

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,11 @@
-import * as React from "react"
-import Layout from "../layouts"
+import * as React from 'react'
+import Layout from '../layouts'
 
-export default () => (
+const NotFoundPage = () => (
   <Layout>
     <h1>You are here!</h1>
     <h2>But nothing found for you #404</h2>
   </Layout>
 )
+
+export default NotFoundPage
